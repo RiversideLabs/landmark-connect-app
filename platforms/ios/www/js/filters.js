98880e6949284324d2a61c27b45e91b85f2561b0
Mike Stecker <mike@stecker.me>
2014-05-14 13:26:10 -0700

    removing build and plugin files, adding default screens and icons

--- platforms/ios/www/js/filters.js
+++
@@ -1,8 +0,0 @@
-angular.module('landmarkConnect.filters', [])
-
-.filter('capitalize', function() {
-    return function(input, scope) {
-        if (input!=null)
-            return input.substring(0,1).toUpperCase()+input.substring(1);
-    }
-})
